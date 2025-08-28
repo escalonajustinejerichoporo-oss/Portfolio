4f5372eb9abe458ea45e298cc436faac014d9a27
escalonajustinejerichoporo-oss <escalonajustinejerichoporo@gmail.com>
2025-08-28 17:56:35 +0800

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,39 @@
+function openBox() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("modal").style.display = "block";
+}
+
+function closeBox() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("modal").style.display = "none";
+}
+
+function openBox2() {
+  document.getElementById("overlay2").style.display = "block";
+  document.getElementById("modal2").style.display = "block";
+}
+
+function closeBox2() {
+  document.getElementById("overlay2").style.display = "none";
+  document.getElementById("modal2").style.display = "none";
+}
+
+function openBox3() {
+  document.getElementById("overlay3").style.display = "block";
+  document.getElementById("modal3").style.display = "block";
+}
+
+function closeBox3() {
+  document.getElementById("overlay3").style.display = "none";
+  document.getElementById("modal3").style.display = "none";
+}
+
+function openBox4() {
+  document.getElementById("overlay4").style.display = "block";
+  document.getElementById("modal4").style.display = "block";
+}
+
+function closeBox4() {
+  document.getElementById("overlay4").style.display = "none";
+  document.getElementById("modal4").style.display = "none";
+}
